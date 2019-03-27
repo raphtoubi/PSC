a5e34ebe15473db99dfbff04e0d25216ff5c7103
Kaldabra <43815572+Kaldabra@users.noreply.github.com>
2019-03-27 16:51:11 +0100

    Add files via upload
    Fonctionnalité supplémentaire :
    freqselect qui sélectionne aléatoirement une fréquence en favorisant la proximité avec la fréquence précédente.
    Ajustement à faire en situation réelle
    
    Cosmétique : 
    Création d'une fonction main pour coordonner plus facilement entre les différentes parties

--- VideoTest.py
+++ VideoTest.py
@@ -1,8 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  6 14:10:44 2019
+
+@author: Alexian
+"""
+
 import numpy as np
 import cv2
 import pywt;
 import matplotlib.pyplot as plt;
 from scipy.fftpack import fft;
+from random import random;
 
 
 def waveTransform2(wavelet,data) :
@@ -17,50 +25,66 @@ def fourierTransform(data) :
     plt.show();"""            "permet d'afficher la transformée de fourier si décommentée"
     return liste2;
 
+def freqselect(listefreq, freqprec) :
+    for i in range(np.size(listefreq)) :
+        listefreq[i] = listefreq[i]/((np.abs(freqprec-i)+1)**3);
+    sumcoeff=np.sum(listefreq);
+    listefreq=listefreq/sumcoeff;
+    r = random();
+    somme=0;
+    res=0;
+    for i in range(np.size(listefreq)) :
+        somme += listefreq[i];
+        if(somme > r) :
+            res=i;
+            break;
+    return res;
 
-cap = cv2.VideoCapture(r'C:\Users\Alexian\Videos\Captures\GFAP8866.MP4');     "récupère la vidéo"
+def main(nomDeLaVideo) :
+    cap = cv2.VideoCapture(nomDeLaVideo);     "récupère la vidéo"
+    m=300;            "nombre d'image considérée lors de la transformée de fourier"
+    i=0;              "pointeur pour la transformée de fourier"
+    tabfreqmax = [];  "stocke l'indice de la valeur maximale renvoyée par la transformée de fourier"
+    data1=[];         "stocke les valeurs renvoyées par waveTransform2 pour une première onde"
+    data2=[];         "deuxième onde"
+    data3=[];         "troisième onde"
+    fps=cap.get(cv2.CAP_PROP_FPS);  "récupère le nombre d'images par seconde"
 
-m=300;            #nombre d'image considérée lors de la transformée de fourier"
-i=0;              #pointeur pour la transformée de fourier"
-tabfreqmax = [];  #stocke l'indice de la valeur maximale renvoyée par la transformée de fourier"
-data1=[];         #stocke les valeurs renvoyées par waveTransform2 pour une première onde"
-data2=[];         #deuxième onde"
-data3=[];         #troisième onde"
-fps=cap.get(cv2.CAP_PROP_FPS);  #récupère le nombre d'images par seconde"
-
-while(True):
-    #lit la vidéo image par image:
-    ret, frame = cap.read();                     
-    #arrête la lecture si la vidéo est terminée
-    if ret==True:
-        b,g,r = cv2.split(frame);                #sépare les couleurs de l'image
-        data1.append(waveTransform2('db2',g));   #rajoute la valeur calculée par waveTransform2 avec la première onde
-        data2.append(waveTransform2('db4',g));   #deuxième onde
-        data3.append(waveTransform2('db7',g));   #troisième onde
-        #arrête la lecture si l'utilisateur appuie sur q:
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-             break
-    else:
-        break
-        
-#récupère le nombre d'image de la vidéo    
-n=len(data1);
-
-#tant que le pointeur + le nombre d'image nécessaire ne dépasse pas le nombre d'image total
-while m+i<n :
-    fourier = fourierTransform(data1[i:m+i]) + fourierTransform(data2[i:m+i]) + fourierTransform(data3[i:m+i]);     #somme les transformées de fourier sur les intervalles considérés
-    tabfreqmax.append(np.argmax(fourier[0:int(m/2)])* 60 / (m / fps));         #calcule et ajoute l'indice du coefficient maximal de la transformée de fourier
-    i=i+1;                                                                     #met à jour le pointeur
-time = np.arange(len(tabfreqmax));                                             #crée un tableau de 0 à len(tabfreqmax) avec un pas de 1
-plt.plot(time, tabfreqmax);   #affiche l'indice du coefficient maximal de la transformée de fourier en fonction de l'image considérée
-plt.xlabel('Image numéro');
-plt.ylabel('Fréquence estimée');
-plt.title('Fréquence cardiaque estimée pour chaque image');
-plt.show()
-cap.release()
-cv2.destroyAllWindows()
-
-
-
-
+    while(True):
+        ret, frame = cap.read();                     "lit la vidéo image par image"
+        "arrête la lecture si la vidéo est terminée"
+        if ret==True:
+            b,g,r = cv2.split(frame);                "sépare les couleurs de l'image"
+            data1.append(waveTransform2('db2',g));   "rajoute la valeur calculée par waveTransform2 avec la première onde"
+            data2.append(waveTransform2('db4',g));   "deuxième onde"
+            data3.append(waveTransform2('db7',g));   "troisième onde"
+            "arrête la lecture si l'utilisateur appuie sur q"
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
+            break
     
+    n=len(data1);                                    "récupère le nombre d'image de la vidéo"
+    fourier = fourierTransform(data1[0:m]) + fourierTransform(data2[0:m]) + fourierTransform(data3[0:m]);
+    i+=1;
+    tabfreqmax.append(np.argmax(fourier[0:int(m/2)])* 60 / (m / fps));
+    "tant que le pointeur + le nombre d'image nécessaire ne dépasse pas le nombre d'image total"
+    while m+i<n :
+        fourier = fourierTransform(data1[i:m+i]) + fourierTransform(data2[i:m+i]) + fourierTransform(data3[i:m+i]);     "somme les transformées de fourier sur les intervalles considérés"
+        fourier[0]=0;
+        fourier[1]=0;                   "évite l'attraction de forts coeffs à faibles fréquences quand l'image est très affectée"
+        fourier[2]=0;
+        tabfreqmax.append(freqselect(fourier[0:int(m/2)],tabfreqmax[-1]/(60 / (m / fps)))* 60 / (m / fps));         "calcule et ajoute l'indice du coefficient maximal de la transformée de fourier"
+        i=i+1;                                                                  "met à jour le pointeur"
+    time = np.arange(len(tabfreqmax));                                          "crée un tableau de 0 à len(tabfreqmax) avec un pas de 1"
+    plt.plot(time, tabfreqmax);   "affiche l'indice du coefficient maximal de la transformée de fourier en fonction de l'image considérée"
+    plt.xlabel('Image numéro');
+    plt.ylabel('Fréquence estimée');
+    plt.title('Fréquence cardiaque estimée pour chaque image');
+    plt.show()
+    cap.release()
+    cv2.destroyAllWindows();
+
+""" ne pas faire attention : stockage en cours
+tabfreqmax.append(np.argmax(fourier[0:int(m/2)])* 60 / (m / fps));
+vidéo pour les tests 'C:/Users/Alexian/Videos/Captures/GFAP8866.MP4' """
